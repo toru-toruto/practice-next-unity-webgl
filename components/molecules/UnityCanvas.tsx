bc62a7554ba340e54c3561a8f5d260c365fe6bcb
toru-toruto <toruto.torutoru.toruto@gmail.com>
2023-10-10 17:02:15 +0000

    fix: path

--- components/molecules/UnityCanvas.tsx
+++ components/molecules/UnityCanvas.tsx
@@ -5,10 +5,11 @@ import { CanvasLoading } from "../atoms/CanvasLoading";
 export const UnityCanvas: React.FC = () => {
   const { unityProvider, isLoaded, loadingProgression, requestFullscreen } =
     useUnityContext({
-      loaderUrl: "/unity-build/simple-cube/Build/build-webgl.loader.js",
-      dataUrl: "/unity-build/simple-cube/Build/build-webgl.data",
-      frameworkUrl: "/unity-build/simple-cube/Build/build-webgl.framework.js",
-      codeUrl: "/unity-build/simple-cube/Build/build-webgl.wasm",
+      loaderUrl: "/unity-build/simple-cube/Build/build-simple-cube.loader.js",
+      dataUrl: "/unity-build/simple-cube/Build/build-simple-cube.data",
+      frameworkUrl:
+        "/unity-build/simple-cube/Build/build-simple-cube.framework.js",
+      codeUrl: "/unity-build/simple-cube/Build/build-simple-cube.wasm",
     });
   const { devicePixelRatio } = useDpr();
 
