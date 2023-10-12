60fef2d90202d11fcfdac2cd44e3558588a4e01b
toru-toruto <toruto.torutoru.toruto@gmail.com>
2023-10-12 01:14:28 +0000

    test: switch to cube static 1000

--- components/molecules/UnityCanvas/UnityCanvas.tsx
+++ components/molecules/UnityCanvas/UnityCanvas.tsx
@@ -4,7 +4,7 @@ import { CanvasLoading } from "../../atoms/CanvasLoading";
 import { resourceData } from "./resourceData";
 
 export const UnityCanvas: React.FC = () => {
-  const { folderName, fileName } = resourceData.testCubeRigidbody1000;
+  const { folderName, fileName } = resourceData.testCubeStatic1000;
 
   const { unityProvider, isLoaded, loadingProgression, requestFullscreen } =
     useUnityContext({
